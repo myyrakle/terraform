eea1d3c3d45a6834ec6c9ffc177c94929c1df47b
myyrakle <sssang97@naver.com>
2023-05-05 17:26:44 +0900

    add: ecr

--- aws/ecs-server/ecs-server.tf
+++ aws/ecs-server/ecs-server.tf
@@ -27,3 +27,25 @@ resource "aws_cloudwatch_log_group" "log_group" {
     Application = var.server_name
   }
 }
+
+// CloudWatch 로그 그룹
+// 서버 로그 기록에 사용합니다.
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = join("-", [var.server_name, var.var.environment])
+  retention_in_days = var.log_retention_in_days
+
+  tags = {
+    Environment = var.environment
+    Application = var.server_name
+  }
+}
+
+// ECR
+// 서버 프로지버닝에 사용할 docker image를 관리합니다.
+resource "aws_ecr_repository" "ecr" {
+  name                 = join("-", [var.server_name, var.environment])
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+  }
+}
