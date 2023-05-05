f1e22ab580a312360d55ad4dee3cdadd0345a3f9
myyrakle <sssang97@naver.com>
2023-05-05 19:06:12 +0900

    add: ecs_service_sg

--- aws/ecs-server/security_group.tf
+++ aws/ecs-server/security_group.tf
@@ -13,3 +13,27 @@ resource "aws_security_group" "loadbalancer_sg" {
 
   tags = local.tags
 }
+
+// ECS Service 보안그룹
+resource "aws_security_group" "ecs_service_sg" {
+  name        = "${local.resource_id}-ecs-service-sg"
+  description = ""
+  vpc_id      = var.vpc_id
+
+  egress {
+    description = "allow all TCP"
+    from_port   = 65535
+    to_port     = 0
+    protocol    = "tcp"
+  }
+
+  ingress {
+    description = ""
+    from_port   = var.portforward_host_port
+    to_port     = var.portforward_host_port
+    protocol    = "tcp"
+  }
+
+  tags = local.tags
+}
+
