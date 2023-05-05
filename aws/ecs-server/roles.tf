fe2f26b6e524308ed5e0b3bb3bac2d21565d2332
myyrakle <sssang97@naver.com>
2023-05-05 22:32:43 +0900

    add: codepipeline_role

--- aws/ecs-server/roles.tf
+++ aws/ecs-server/roles.tf
@@ -182,3 +182,61 @@ resource "aws_iam_role" "codedeploy_role" {
   tags = local.tags
 }
 
+// Code Pipeline에 사용할 role
+resource "aws_iam_role" "codepipeline_role" {
+  name = join("-", [var.server_name, var.environment, "codepipeline-role"])
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "codepipeline.amazonaws.com"
+        },
+        "Action" : ["sts:AssumeRole"]
+      }
+    ]
+  })
+
+  path = "/"
+
+  inline_policy {
+    name    = "root"
+    version = "2012-10-17"
+    statement = [
+      {
+        "Resource" : {
+          "Fn::Sub" : "arn:aws:s3:::${ArtifactBucket}/*"
+        },
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning"
+        ]
+      },
+      {
+        "Resource" : "*",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:ListTasks",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService",
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds",
+          "iam:PassRole"
+        ]
+      }
+    ]
+  }
+
+  tags = local.tags
+}
+
