3906d35d3e4cb6bcc33b3ba08b010bb74e8aae50
myyrakle <sssang97@naver.com>
2023-05-07 13:14:20 +0900

    add: lambda role

--- aws/lambder-server/function_url/main.tf
+++ aws/lambder-server/function_url/main.tf
@@ -13,3 +13,20 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+resource "aws_lambda_function" "lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = local.resource_id
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {}
+  }
+}
---
+++ aws/lambder-server/function_url/roles.tf
@@ -0,0 +1,18 @@
+// Lambda Role
+resource "aws_iam_role" "lambda_role" {
+  name = local.resource_id
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : ["ecs-tasks.amazonaws.com"]
+        },
+        "Action" : [
+          "sts:AssumeRole"
+        ]
+      }
+    ]
+  })
+}
