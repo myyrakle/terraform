fad061c0454b16fcabea3acbed0b341eac300429
myyrakle <sssang97@naver.com>
2023-05-07 13:42:58 +0900

    add: SpecificTable policy

---
+++ aws/lambder-server/function_url/dynamo.tf
@@ -0,0 +1,24 @@
+# 테이블을 여기에서 정의합니다.
+
+// 유저 테이블
+resource "aws_dynamodb_table" "user-table" {
+  name         = "${local.resource_id}-user"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "uuid"
+  # range_key    = ""
+
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+
+  // 글로벌 보조 인덱스
+  global_secondary_index {
+    name     = "email-index"
+    hash_key = "email"
+    // range_key          = ""
+    projection_type = "ALL"
+  }
+
+  tags = local.tags
+}
--- aws/lambder-server/function_url/roles.tf
+++ aws/lambder-server/function_url/roles.tf
@@ -15,9 +15,31 @@ resource "aws_iam_role" "lambda_role" {
       }
     ]
   })
-}
 
-resource "aws_iam_role_policy_attachment" "dynamofullaccess-attach" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  inline_policy {
+    name = "root"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          "Sid" : "SpecificTable",
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:BatchGet*",
+            "dynamodb:DescribeStream",
+            "dynamodb:DescribeTable",
+            "dynamodb:Get*",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:BatchWrite*",
+            "dynamodb:CreateTable",
+            "dynamodb:Delete*",
+            "dynamodb:Update*",
+            "dynamodb:PutItem"
+          ],
+          "Resource" : "arn:aws:dynamodb:*:*:table/${local.resource_id}*"
+        }
+      ]
+    })
+  }
 }
