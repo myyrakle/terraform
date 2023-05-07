3a7d2b1200920fae6b898dcb928e8e684440a5ac
myyrakle <sssang97@naver.com>
2023-05-07 15:27:04 +0900

    add: function_name

--- aws/lambder-server/function_url/outputs.tf
+++ aws/lambder-server/function_url/outputs.tf
@@ -2,3 +2,8 @@ output "function_url" {
   value       = aws_lambda_function_url.release_url.function_url
   description = "Function Url"
 }
+
+output "function_name" {
+  value       = aws_lambda_function_url.release_url.function_name
+  description = "Function Name"
+}
