65e99440bdd4ab4bbcbf61346852505a05b2cd09
myyrakle <sssang97@naver.com>
2023-05-07 13:07:00 +0900

    init: main

---
+++ aws/lambder-server/function_url/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    # 일종의 라이브러리 로드
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
